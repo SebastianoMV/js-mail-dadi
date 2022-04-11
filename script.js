81877b40428db0e4df10acd4b1187a74847315f4
SebastianoMV <sebastianovisco@gmail.com>
2022-04-11 16:33:20 +0200

    Dadi

--- script.js
+++ script.js
@@ -18,12 +18,38 @@ btnStartEmail.addEventListener('click', function(){
       presenza = true;
     }
   };
- 
-  
   if (presenza == true){
     risultato.innerHTML = 'Benvenuto!'
   }else{
     risultato.innerHTML = 'Non sei registrato'
   }
-
 });
+
+
+//1 genera un numero random per il giocatore dopo avere premuto un bottone e uno per il pc
+// 2 confrontare i due numeri per stabilire il vincitore
+// 3 mostrare a video il vincitore
+const btnStartDadi = document.querySelector('#btnDadi');
+const dadoUtente = document.querySelector('.dadoUt');
+const dadoPc = document.querySelector('.dadoPC');
+const risultatoDadi = document.querySelector('h2');
+
+btnStartDadi.addEventListener('click', function(){
+  let numeroUt = Math.floor(Math.random() * 6); 
+  let numeroPC = Math.floor(Math.random() * 6);
+  dadoUtente.innerHTML = numeroUt;
+  dadoPc.innerHTML = numeroPC;
+
+  if (numeroUt < numeroPC){
+    risultatoDadi.innerHTML = 'Hai perso!';
+  } else if (numeroUt > numeroPC){
+    risultatoDadi.innerHTML = 'Hai Vinto!';
+  } else{
+    risultatoDadi.innerHTML = 'Pareggio!';
+  }
+  
+});
+
+
+
+
